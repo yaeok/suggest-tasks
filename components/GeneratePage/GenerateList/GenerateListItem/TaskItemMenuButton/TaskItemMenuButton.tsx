408f0aa51e7b437e83fc72eb9df71fbecbd00fa7
yaeok <k.yaeo@carep-tech.com>
2024-12-30 10:17:13 +0900

    deployエラーの解消

--- components/GeneratePage/GenerateList/GenerateListItem/TaskItemMenuButton/TaskItemMenuButton.tsx
+++ components/GeneratePage/GenerateList/GenerateListItem/TaskItemMenuButton/TaskItemMenuButton.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import { RxHamburgerMenu } from 'react-icons/rx'
 
 export default function TaskItemMenuButton() {
   const [isOpen, setIsOpen] = useState(false)
