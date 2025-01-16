2f727f9780b77e3d6eca725fdbd2eb2daf46ddf5
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2025-01-16 10:20:30 +0900

    20250116作業分

--- app/(service)/tasks/[slug]/_components/TaskItemListItem/TaskItemListItem.tsx
+++ app/(service)/tasks/[slug]/_components/TaskItemListItem/TaskItemListItem.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { IconContext } from 'react-icons'
-import { BiLogoFlutter } from 'react-icons/bi'
+import { SiNextdotjs } from 'react-icons/si'
 
 import { TaskItem } from '@/model/TaskItem'
 
@@ -18,44 +18,45 @@ export default function TaskItemListItem({
   return (
     <section
       key={index}
-      className='w-full p-6 bg-white rounded-lg border-l-8 border-blue-500 flex flex-col justify-between items-center gap-4'
+      className='w-full p-6 bg-white rounded-lg border-l-8 border-blue-500 flex flex-col justify-between items-center gap-4 relative'
     >
-      <section className='w-full flex flex-col items-center gap-8'>
-        <div className='w-full flex justify-between'>
-          <div className='relative aspect-square p-4 border-2 border-blue-800 rounded-full flex justify-center items-center'>
-            <span className='absolute text-sm font-semibold'>{index + 1}</span>
-          </div>
-          <TaskItemMenuButton />
+      <div className='absolute top-5 right-5'>
+        <TaskItemMenuButton />
+      </div>
+      <div className='w-full flex flex-row items-center gap-8'>
+        <div className='w-1/3 flex justify-center items-center'>
+          <IconContext.Provider value={{ size: '7em' }}>
+            <SiNextdotjs />
+          </IconContext.Provider>
         </div>
-        <IconContext.Provider value={{ size: '5em' }}>
-          <BiLogoFlutter />
-        </IconContext.Provider>
-        <div className='flex flex-col justify-between items-center gap-4'>
-          <h1 className='text-lg font-semibold text-black'>{task.title}</h1>
-          <div className='flex flex-col items-start gap-2'>
-            <p className='text-sm border-b border-black'>学習予定期間</p>
-            <h2 className='text-sm font-semibold text-gray-400'>
-              {task.startDate.toLocaleDateString()} ~{' '}
-              {task.endDate.toLocaleDateString()}
-            </h2>
+        <div className='w-2/3 flex flex-col gap-4 items-start'>
+          <div className='flex flex-col justify-between gap-4'>
+            <h1 className='text-xl font-extrabold text-black'>{task.title}</h1>
+            <div className='flex flex-col items-start gap-2'>
+              <p className='text-sm border-b border-black'>学習予定期間</p>
+              <h2 className='text-sm font-semibold text-gray-400'>
+                {task.startDate.toLocaleDateString()} ~{' '}
+                {task.endDate.toLocaleDateString()}
+              </h2>
+            </div>
+          </div>
+          <p className='text-sm font-normal text-black'>{task.content}</p>
+          <div className='flex flex-row justify-between items-center gap-4'>
+            <span className='text-sm font-semibold text-gray-400'>
+              {task.duration}時間
+            </span>
+            {task.reference && (
+              <Link
+                href={task.reference}
+                target='_blank'
+                className='text-sm font-semibold text-blue-500'
+              >
+                参考資料
+              </Link>
+            )}
           </div>
         </div>
-        <p className='text-sm font-normal text-black'>{task.content}</p>
-      </section>
-      <section className='flex flex-row justify-between items-center gap-4'>
-        <span className='text-sm font-semibold text-gray-400'>
-          {task.duration}時間
-        </span>
-        {task.reference && (
-          <Link
-            href={task.reference}
-            target='_blank'
-            className='text-sm font-semibold text-blue-500'
-          >
-            参考資料
-          </Link>
-        )}
-      </section>
+      </div>
     </section>
   )
 }
